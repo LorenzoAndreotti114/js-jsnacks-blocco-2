c5dbb891b1a6b7eb780b1647fa017de172ad4da1
LorenzoAndreotti114 <lorenzoandr123@gmail.com>
2024-04-06 15:57:57 +0200

    snack-3 final project

---
+++ snack-3/js/functions.js
@@ -0,0 +1,9 @@
+function arrayStamper (array) {
+
+    let result
+
+    result = array
+
+    return result
+
+}
---
+++ snack-3/js/script.js
@@ -0,0 +1,5 @@
+let arrayTest = ["pippo", "pluto", "paperino", "olga"];
+
+arrayStamper (arrayTest)
+
+document.getElementById("stamper").innerHTML = arrayStamper(arrayTest)
